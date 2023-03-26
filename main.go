87479d603e49abfec196e9f39f05c69878935991
Văn Thanh <vanthanh@jv-it.com.vn>
2023-03-26 19:03:30 +0700

    code CRUD API postman

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 type Restaurant struct {
@@ -36,6 +39,136 @@ func main()  {
 		log.Fatalln(err)
 	}
 
+	log.Println(db)
+
+	r := gin.Default() // server connection
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{ // H là map []
+			"message": "pong",
+		})
+	})
+	// POST restaurant
+	v1 := r.Group("/v1")
+
+	restaurants := v1.Group("/restaurants")
+
+	restaurants.POST("", func(c *gin.Context) {
+		var data Restaurant
+
+		if err := c.ShouldBind(&data); err != nil {
+			// ShouldBind: dùng để đọc và gán giá trị từ các request parameter vào các struct
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		db.Create(&data)
+		c.JSON(http.StatusOK, gin.H{
+			"data" : data,
+		})
+	})
+
+	// get by id
+	restaurants.GET("/:id", func(c *gin.Context) {
+		// get id use c.Params
+		id, err := strconv.Atoi(c.Param("id")) // Atoi: chuyển đổi một chuỗi số sang dạng số nguyên (int)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+                "error": err.Error(),
+            })
+            return
+		}
+		var data Restaurant
+
+		db.Where("id = ?", id).First(&data)
+		c.JSON(http.StatusOK, gin.H{
+			"data" : data,
+		})
+	})
+
+	// get all
+	restaurants.GET("", func(c *gin.Context) {
+		var data []Restaurant
+
+		// pagging data
+		type Paging struct {
+			Page int `json:"page" form:"page"`
+		    Limit int `json:"limit" form:"limit"`
+        }
+
+        var pagingData Paging
+
+		if err := c.ShouldBind(&pagingData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if pagingData.Page <= 0 {
+			pagingData.Page = 1
+		}
+		if pagingData.Limit <= 0 {
+            pagingData.Limit = 5
+        }
+
+		db.Offset((pagingData.Page - 1 ) * pagingData.Limit).
+			Order("id desc").
+			Limit(pagingData.Limit).
+			Find(&data)
+
+		c.JSON(http.StatusOK, gin.H{
+			"data" : data,
+		})
+	})
+
+	// update by id
+	restaurants.PUT("/:id", func(c *gin.Context) { // use postman nhớ chuyển sang kiểu json
+		// get id use c.Params
+		id, err := strconv.Atoi(c.Param("id")) // Atoi: chuyển đổi một chuỗi số sang dạng số nguyên (int)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		var data RestaurantUpdate
+
+		if err := c.ShouldBind(&data); err != nil {
+			// ShouldBind: dùng để đọc và gán giá trị từ các request parameter vào các struct
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		db.Where("id = ?", id).Updates(&data)
+		c.JSON(http.StatusOK, gin.H{
+			"status": 1,
+			"data" : data,
+		})
+	})
+
+	// delete by id
+	restaurants.DELETE("/:id", func(c *gin.Context) {
+		// get id use c.Params
+		id, err := strconv.Atoi(c.Param("id")) // Atoi: chuyển đổi một chuỗi số sang dạng số nguyên (int)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		db.Table(Restaurant{}.TableName()).Where("id = ?", id).Delete(nil)
+		c.JSON(http.StatusOK, gin.H{
+			"data" : "Delete successful",
+		})
+	})
+
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 	//newRestaurant := Restaurant{
 	//	Name: "megumi fushigoro",
 	//	Addr: "Shukuna",
@@ -48,30 +181,30 @@ func main()  {
 	//log.Println("New success Id = ", newRestaurant.Id)
 
 	// ----------------------------Read--------------------------------
-	var restaurants Restaurant
-    if err := db.Where("id = ?", 6).Find(&restaurants).Error; err!= nil {
-        log.Println(err)
-    }
-
-    log.Println(restaurants)
-
-    // ----------------------------Update--------------------------------
-	restaurants.Name = "Megumi Fushigoro"
-	if err := db.Where("id = ?", 6).Updates(&restaurants).Error; err!= nil {
-		log.Println(err)
-	}
-	log.Println(restaurants)
-
-	// ----------------------------Update name = nil ~~ ""--------------------------------
-	newName := "ThanhRain"
-	updateData := RestaurantUpdate{Name: &newName}
-	if err := db.Where("id = ?", 3).Updates(&updateData).Error; err!= nil {
-		log.Println(err)
-	}
-	log.Println(restaurants)
-	// ----------------------------Delete--------------------------------
-	if err := db.Table(Restaurant{}.TableName()).Where("id =?", 1).Delete(nil).Error; err!= nil {
-        log.Println(err)
-    }
-    log.Println(restaurants)
+	//var restaurants Restaurant
+    //if err := db.Where("id = ?", 6).Find(&restaurants).Error; err!= nil {
+    //    log.Println(err)
+    //}
+	//
+    //log.Println(restaurants)
+	//
+    //// ----------------------------Update--------------------------------
+	//restaurants.Name = "Megumi Fushigoro"
+	//if err := db.Where("id = ?", 6).Updates(&restaurants).Error; err!= nil {
+	//	log.Println(err)
+	//}
+	//log.Println(restaurants)
+	//
+	//// ----------------------------Update name = nil ~~ ""--------------------------------
+	//newName := "ThanhRain"
+	//updateData := RestaurantUpdate{Name: &newName}
+	//if err := db.Where("id = ?", 3).Updates(&updateData).Error; err!= nil {
+	//	log.Println(err)
+	//}
+	//log.Println(restaurants)
+	//// ----------------------------Delete--------------------------------
+	//if err := db.Table(Restaurant{}.TableName()).Where("id =?", 1).Delete(nil).Error; err!= nil {
+    //    log.Println(err)
+    //}
+    //log.Println(restaurants)
 }
