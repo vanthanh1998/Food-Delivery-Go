447f23612d7aa3908ad0e826afcc741e3fc52f63
Van Thanh <thanh.tong@emart.vn>
2023-04-30 17:16:15 +0700

    find find

--- module/restaurantlike/storage/find.go
+++ module/restaurantlike/storage/find.go
@@ -1,1 +1,27 @@
 package restaurantlikestorage
+
+import (
+	"Food-Delivery/common"
+	restaurantmodel "Food-Delivery/module/restaurant/model"
+	"context"
+	"gorm.io/gorm"
+)
+
+func (s *sqlStore) FindDataWithCondition(
+	context context.Context,
+	condition map[string]interface{}, // map[key]value
+	moreKeys ...string,
+) (*restaurantmodel.Restaurant, error) {
+	// find nhà hàng đó đã được like hay chưa
+	var data restaurantmodel.Restaurant
+	db := s.db
+
+	if err := db.Where(condition).First(&data).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, common.RecordNotFound
+		}
+		return nil, common.ErrDB(err)
+	}
+
+	return &data, nil
+}
